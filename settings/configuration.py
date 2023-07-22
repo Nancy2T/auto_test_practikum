e655eee06a36a51819903f8788ff7c2b3c9dca68
Nancy2T <110238709+Nancy2T@users.noreply.github.com>
2023-07-22 15:43:00 +0300

    Create configuration.py

---
+++ settings/configuration.py
@@ -0,0 +1,4 @@
+SERVICE_URL = 'https://00dc4875-b6a5-4607-9029-8c79f95d7bc7.serverhub.praktikum-services.ru/'
+CREATE_ORDER_PATH = '/api/v1/orders'
+GET_ORDER_PATH = '/api/v1/orders/track'
+TEST_LOCAL = 'ru-RU'
